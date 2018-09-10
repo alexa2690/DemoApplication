1078813445625b100bb30f5da2ab79e9c9d0de91
Crisan Alexandra <alexandra.crisan@tss-yonder.com>
2018-09-10 12:19:58 +0300

    Updated message

--- DemoApplication/Program.cs
+++ DemoApplication/Program.cs
@@ -6,7 +6,7 @@ namespace DemoApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wordgagald!");
         }
     }
 }
